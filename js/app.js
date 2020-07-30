658206c2fa2bab28a5f8cff121309fac01aa95df
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-07-30 02:29:34 +0100

    new changes

--- js/app.js
+++ js/app.js
@@ -14,23 +14,31 @@ let trackq = 1;
 let scorecount = 0;
 let questions
 let checkanswers
+let trackClick
 
 
 const action = () => {
     res.classList.remove("stop");
     if (questions.length === 0) {
-        game.classList.add("none");
-        total.textContent = scorecount
-        pg.classList.remove("none");
+        if (trackClick === true) {
+            game.classList.add("none");
+            total.textContent = scorecount
+            pg.classList.remove("none");
+            trackClick = false;
+        }
     } else {
-        let pop = questions.pop()
-        track++
-        trackq++
-        qid.textContent = trackq;
-        let popanswers = pop.answers;
-        que.innerHTML = "<p class='que'>" + pop.question + "</p>"
-        res.innerHTML = "<p class='ans'>" + popanswers[0] + "</p>" + "<p class='ans'>" + popanswers[1] +
-            "</p>" + "<p class='ans'>" + popanswers[2] + "</p>" + "<p class='ans'>" + popanswers[3] + "</p>"
+        if (trackClick === true) {
+            let pop = questions.pop()
+            track++
+            trackq++
+            qid.textContent = trackq;
+            let popanswers = pop.answers;
+            que.innerHTML = "<p class='que'>" + pop.question + "</p>"
+            res.innerHTML = "<p class='ans'>" + popanswers[0] + "</p>" + "<p class='ans'>" + popanswers[1] +
+                "</p>" + "<p class='ans'>" + popanswers[2] + "</p>" + "<p class='ans'>" + popanswers[3] + "</p>"
+            trackClick = false;
+        }
+
     }
 
 }
@@ -80,17 +88,19 @@ const action3 = (evt) => {
             res.classList.add("stop");
             scorecount++;
             score.textContent = scorecount;
+            trackClick = true
         } else {
             evt.target.classList.add("wrong");
             let ans = document.querySelectorAll(".ans");
             ans = Array.from(ans);
             ans.map((item) => {
-                if(item.textContent === checkanswers[track]){
+                if (item.textContent === checkanswers[track]) {
                     item.classList.add("right");
                 }
             })
 
             res.classList.add("stop");
+            trackClick = true
         }
     }
 
